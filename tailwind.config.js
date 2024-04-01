d15008c1024f327e2fe710678da7be8a1b8281c3
ineedlove271002 <bao36987@gmail.com>
2024-04-02 00:04:34 +0700

    front-end

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "banner-bg": "url('../public/images/bannerBg.jpeg')",
+      },
+    },
   },
   plugins: [],
   variants: {
